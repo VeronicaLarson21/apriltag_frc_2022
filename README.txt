8555319f17230c1c75d12f4cbc468752e9c4e4e2
TheWalkigDude <finnlarson21@gmail.com>
2022-09-24 00:48:50 -0400

    init commit

---
+++ README.txt
@@ -0,0 +1,5 @@
+Preseason project using python for apriltag detection and sent for use in main robot robot code in the face of the addition to the 2022-2023 game
+
+Use my code if you want its probally worse than googling the keywords
+
+Good Luck to me
---
+++ detection_from_live_image_factory_wrapper.py
@@ -0,0 +1,32 @@
+import cv2
+from apriltag import apriltag
+
+
+# setup and the main loop
+detector = apriltag("tag36h11")
+cam = cv2.VideoCapture(0)
+
+looping = True
+
+while looping:
+    result, image = cam.read()
+    grayimg = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	# look for tags
+    detections = detector.detect(grayimg)
+    if not detections:
+        print("Nothing")
+    else:
+        print("Tag found!")
+        looping = False
+    cv2.imshow('Result', image)
+	
+
+# loop over; clean up and dump the last updated frame for convenience of debugging
+cv2.destroyAllWindows()
+cv2.imwrite("final.png", image)
+
+# let the system event loop do its thing
+     # key = cv2.waitKey(100)
+	# terminate the loop if the 'Return' skey his hit
+    # if key == 13:
+        # looping = False
