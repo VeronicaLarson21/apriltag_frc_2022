dee387ab0cd4841118198444f597c2884478bce8
TheWalkigDude <finnlarson21@gmail.com>
2022-10-20 22:22:50 -0400

    added frc specific file, updated readme

--- README.txt
+++ README.txt
@@ -1,5 +1,6 @@
 Preseason project using python for apriltag detection and sent for use in main robot robot code in the face of the addition to the 2022-2023 game
-
 Use my code if you want its probally worse than googling the keywords
+This project is composed of 3 files, a simple detection loop using the factory standard Apriltags python library, a simple detection loop using the using the pupil apirltags python wrapper around apriltags with corner detection(more can be added for demo), and finally the bulk of the project, the frc specific detection+sending to robot file.
+
 
 
--- tag_to_pi_to_bot.py
+++ tag_to_pi_to_bot.py
@@ -1,13 +1,12 @@
+from cscore import CameraServer
 from pupil_apriltags import Detector
 import cv2
-
-#this is a test using one of the many python wrappers around the factory standard one bc that ones documentation sucks donkey butt
+from networktables import NetworkTables
 # setup and the main loop
 detector = Detector(families='tag36h11')
 cam = cv2.VideoCapture(0)
-
+Networktables.intialize('10.41.22.2')
 looping = True
-
 while looping:
     result, image = cam.read()
     grayimg = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -15,7 +14,7 @@ while looping:
     detections = detector.detect(grayimg)
     #make this better than jamming my screen with nothings, gonna put nothing in there maybe that will work? nah i want a way to tell no matter how annoying
     if not detections:
-        print("Nothing")
+        pass
     else:
         #loops over detections lets me get at the other results beyond pure number of detections
         for r in detections:
@@ -23,9 +22,9 @@ while looping:
             print(r.corners)
             #print(r.center)
             #set the values we need here(including pose if i get to that, then send those vals over network tables)
-        looping = False
+        #looping = False
 
-    cv2.imshow('Result', image)
+    #cv2.imshow('Result', image) commented for now as will not be needed in final program waste of cpu to have it produce a graphic no soul will see
 
 #breaks the window and ends the program
 cv2.destroyAllWindows()
